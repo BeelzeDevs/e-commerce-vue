ef167b038329b12e9553dc752412d9783bcb4d2b
Ignacio Aprile <ignacioaprile@hotmail.com>
2025-12-30 20:18:27 -0300

    carrousel actualizado 3

--- frontend/src/components/user/HomeCarrouselProd.vue
+++ frontend/src/components/user/HomeCarrouselProd.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="block w-full px-40 pt-32 pb-10">
+  <div class="block w-full px-40 pt-32 pb-10 relative" v-if="loading">
     <div class="flex justify-center items-center w-full">
-      <div v-if="loading" class="loading-container mx-10 my-10">
+      <div class="loading-container">
         <div  class="loading"></div>
         <div class="loading-text text-white">Cargando...</div>
       </div>
